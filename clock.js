8533083bbc16e30c23db385e365924a98c59a546
Damon Achey <Damon@Achey.Net>
2018-03-16 21:37:01 -0600

    move locations to clock objects

--- clock.js
+++ clock.js
@@ -1,20 +1,32 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 10;
+canvas.addEventListener('click', clickHandler);
+
+var ctx = canvas.getContext("2d");
 
 var border = 5;
 var timeControl = 10;
 
-var t1p1 = { time: timeControl, active: true };
-var t1p2 = { time: timeControl };
-var t2p1 = { time: timeControl };
-var t2p2 = { time: timeControl };
+var width = canvas.width / 2 - 2 * border;
+var height = canvas.height / 2 - 2 * border;
+
+var t1p1 = { time: timeControl, x: border, y: border, width: width, height: height };
+var t1p2 = { time: timeControl, x: canvas.width / 2 + border, y: border, width: width, height: height };
+var t2p1 = { time: timeControl, x: border, y: canvas.height / 2 + border, width: width, height: height };
+var t2p2 = { time: timeControl, w: canvas.width / 2 + border, y: canvas.height / 2 + border, width: width, height: height };
+
+t1p1.active = true;
 
 drawClocks();
 
 setInterval(updateClocks, 1000);
+
+function clickHandler(event) {
+  console.log(event);
+  
+  var region
+}
 
 function updateClocks() {
   if (t1p1.time > 0 && t1p1.active) t1p1.time--;
@@ -26,16 +38,13 @@ function updateClocks() {
 }
 
 function drawClocks() {
-	var width = canvas.width / 2 - 2 * border;
-  var height = canvas.height / 2 - 2 * border;
-
-  drawClock(t1p1, border, border, width, height, 180);
-	drawClock(t1p2, canvas.width / 2 + border, border, width, height, 180);
-	drawClock(t2p1, border, canvas.height / 2 + border, width, height, 0);
-	drawClock(t2p2, canvas.width / 2 + border, canvas.height / 2 + border, width, height, 0);
+  drawClock(t1p1);
+	drawClock(t1p2);
+	drawClock(t2p1);
+	drawClock(t2p2);
 }
   
-function drawClock(clock, x, y, w, h, r) {
+function drawClock(clock, x, y, w, h) {
   var minutes = parseInt(clock.time / 60, 10);
   var seconds = parseInt(clock.time % 60, 10);
   
@@ -47,10 +56,10 @@ function drawClock(clock, x, y, w, h, r) {
   ctx.fillStyle = "lightgray";
   if (clock.active) ctx.fillStyle = "green";
   if (clock.time === 0) ctx.fillStyle = "red";
-  ctx.fillRect(x, y, w, h);
+  ctx.fillRect(clock.x, clock.y, clock.width, clock.height);
   ctx.fillStyle = "black";
   ctx.font = canvas.width * 0.15 + "px arial";
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
-  ctx.fillText(text, x + w / 2, y + h / 2);
+  ctx.fillText(text, clock.x + clock.width / 2, clock.y + clock.height / 2);
 }
