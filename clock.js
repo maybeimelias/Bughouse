49f9bc4d5a9e5930108a732b1361a26eecc13dfa
Damon Achey <Damon@Achey.Net>
2018-03-16 21:16:05 -0600

    Add colors and active status

--- clock.js
+++ clock.js
@@ -7,48 +7,50 @@ canvas.height = window.innerHeight - 10;
 var border = 5;
 var timeControl = 10;
 
-var t1p1 = timeControl;
-var t1p2 = timeControl;
-var t2p1 = timeControl;
-var t2p2 = timeControl;
+var t1p1 = { time: timeControl, active: true };
+var t1p2 = { time: timeControl };
+var t2p1 = { time: timeControl };
+var t2p2 = { time: timeControl };
 
 drawClocks();
 
 setInterval(updateClocks, 1000);
 
 function updateClocks() {
-  if (t1p1 > 0) t1p1--;
-  if (t1p2 > 0) t1p2--;
-  if (t2p1 > 0) t2p1--;
-  if (t2p2 > 0) t2p2--;
+  if (t1p1.time > 0 && t1p1.active) t1p1.time--;
+  if (t1p2.time > 0 && t1p2.active) t1p2.time--;
+  if (t2p1.time > 0 && t2p1.active) t2p1.time--;
+  if (t2p2.time > 0 && t2p2.active) t2p2.time--;
   
   drawClocks();
 }
 
 function drawClocks() {
-  var width = canvas.width / 2 - 2 * border;
+	var width = canvas.width / 2 - 2 * border;
   var height = canvas.height / 2 - 2 * border;
 
   drawClock(t1p1, border, border, width, height, 180);
-  drawClock(t1p2, canvas.width / 2 + border, border, width, height, 180);
-  drawClock(t2p1, border, canvas.height / 2 + border, width, height, 0);
-  drawClock(t2p2, canvas.width / 2 + border, canvas.height / 2 + border, width, height, 0);
+	drawClock(t1p2, canvas.width / 2 + border, border, width, height, 180);
+	drawClock(t2p1, border, canvas.height / 2 + border, width, height, 0);
+	drawClock(t2p2, canvas.width / 2 + border, canvas.height / 2 + border, width, height, 0);
 }
   
 function drawClock(clock, x, y, w, h, r) {
-  var minutes = parseInt(clock / 60, 10);
-  var seconds = parseInt(clock % 60, 10);
+  var minutes = parseInt(clock.time / 60, 10);
+  var seconds = parseInt(clock.time % 60, 10);
   
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
   
-  var time = minutes + ":" + seconds;
+  var text = minutes + ":" + seconds;
 
-  ctx.fillStyle = "#aaa";
+  ctx.fillStyle = "lightgray";
+  if (clock.active) ctx.fillStyle = "green";
+  if (clock.time === 0) ctx.fillStyle = "red";
   ctx.fillRect(x, y, w, h);
   ctx.fillStyle = "black";
-  ctx.font = canvas.width * 0.10 + "px arial";
+  ctx.font = canvas.width * 0.15 + "px arial";
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
-  ctx.fillText(time, x + w / 2, y + h / 2);
+  ctx.fillText(text, x + w / 2, y + h / 2);
 }
